2ceabf7959bded3b8923b604b4abbd4c4a9481c4
code-42 <edwd42@gmail.com>
2017-01-31 07:50:13 -0500

    improved build_form() to redisplay when valid=False

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ page_header = """
 <html>
 <head>
     <title>User Signup</title>
+    <style type="text/css">.error {color: red;}</style>
 </head>
 <body>
-    <h1>User Signup</h1>
 """
 
 # html boilerplate for the bottom of every page
@@ -19,15 +19,10 @@ page_footer = """
 """
 
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, username="", password="", verify="", email=""):
-        self.response.out.write(build_page("") % {"username":username,
-                                                  "password":password,
-                                                  "verify":verify,
-                                                  "email":email})
-
     def get(self):
-        content = build_page("")
-        self.response.write(content)
+        page_form = self.build_form()
+        page_content = page_header + page_form + page_footer
+        self.response.write(page_content)
 
     def post(self):
         username = self.request.get('username')
@@ -35,60 +30,73 @@ class MainHandler(webapp2.RequestHandler):
         verify = self.request.get('verify')
         email = self.request.get('email')
 
-        v_username = valid_username(username)
-        v_password = valid_username(password)
-        v_verify = valid_username(verify)
-        v_email = valid_username(email)
+#        v_username = valid_username(username)
+#        v_password = valid_username(password)
+#        v_verify = valid_username(verify)
+#        v_email  = valid_username(email)
 
-        #verify all 3 required form fields were filled in correctly
-        if not(v_username and v_password and v_verify and v_email):
-            self.write_form(username, password, verify, email)
-            self.response.out.write("Please fill in the form completely!")
+        #verify 3 required fields were filled in correctly
+
+#        if (v_username and v_password and v_verify):
+        if (username == ""):
+            #self.rewrite_form(username, password, verify, email)
+            self.response.write(self.build_form(username=username, password=password,verify=verify,email=email))
+            #self.redirect("/?error=" + "error message")
         else:
-            self.response.out.write("Thanks, it works!")
+            self.redirect("/welcome")
 
 
-def build_page(signup_form):
-        header = "<h2>Signup</h2>"
-        username_label = "<label>Username:</label>"
-        username_input = "<input type='text' name='username' value='%(username)s'/><br>"
-        password_label = "<label>Password:</label>"
-        password_input = "<input type='password' name='password' value='%(password)s'/><br>"
-        verify_label = "<label>Verify Password:</label>"
-        verify_input = "<input type='password' name='verify' value='%(verify)s'/><br>"
-        email_label = "<label>Email (optional):</label>"
-        email_input = "<input type='text' name='email' value='%(email)s'/><br>"
-        submit = "<input type='submit'/>"
-        form = ("<form action='' method='post'>" +
-                username_label + username_input + "<br>" +
-                password_label + password_input + "<br>" +
-                verify_label + verify_input + "<br>" +
-                email_label + email_input + "<br>" +
-                submit + "</form>")
+#signup_form = MainHandler()
 
-        return header + form
+    def build_form(self, username="", password="", verify="", email="", error=""):
+            header = "<h1>Signup</h1>"
+            username_label = "<label>Username:</label>"
+            username_input = "<input type='text' name='username' value=''/><span class='error'>%(error)s</span><br>"
+            password_label = "<label>Password:</label>"
+            password_input = "<input type='password' name='password' value=''/><span class='error'>%(error)s</span><br>"
+            verify_label = "<label>Verify Password:</label>"
+            verify_input = "<input type='password' name='verify' value=''/><span class='error'>%(error)s</span><br>"
+            email_label = "<label>Email (optional):</label>"
+            email_input = "<input type='text' name='email' value=''/><span class='error'>%(error)s</span><br>"
+            submit = "<input type='submit'/>"
+            form = "<form action='/' method='post'>" +\
+                username_label + username_input + "<br>" +\
+                password_label + password_input + "<br>" +\
+                verify_label + verify_input + "<br>" +\
+                email_label + email_input + "<br>" +\
+                submit + "</form>"
+
+            return header + form % {"username":username, "password":password,
+                                      "verify":verify,
+                                      "email":email,
+                                      "error":error}
 
 def valid_username(username):
     if username:
-        #x_username = cgi.escape(username, quote=True)
-        return username
+        x_username = cgi.escape(username, quote=True)
+        return x_username
 
 def valid_password(password):
     if password:
-        #x_username = cgi.escape(username, quote=True)
-        return password
+        x_password = cgi.escape(password, quote=True)
+        return x_password
 
 def valid_verify(verify):
     if verify:
-        #x_username = cgi.escape(username, quote=True)
-        return verify
+        x_verify = cgi.escape(verify, quote=True)
+        #if password == verify:
+        return x_verify
 
 def valid_email(email):
     if email:
-        #x_username = cgi.escape(username, quote=True)
-        return email
+        x_email = cgi.escape(email, quote=True)
+        return x_email
 
+class WelcomePage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Welcome ")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomePage)
 ], debug=True)
